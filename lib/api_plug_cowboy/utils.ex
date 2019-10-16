e6bc36339152188621ef8b860962874b575177f6
Fakhri Fauzan <fakhri.fauzan@go-jek.com>
2019-10-16 14:23:52 +0700

    refactoring error utils

--- lib/api_plug_cowboy/utils.ex
+++ lib/api_plug_cowboy/utils.ex
@@ -3,29 +3,16 @@ defmodule ApiPlugCowboy.Utils do
     %{success: true, data: body}
   end
 
-  def format_failure_response(errors) do
-    %{errors: Enum.map(errors, &format_error(&1)), success: false}
+  def format_failed_response(errors) do
+    %{success: false, errors: translate_errors(errors)}
   end
 
-  def format_failure_response(title, msg), do: format_failure_response([{title, msg}])
-
+  # ref : https://hexdocs.pm/ecto/Ecto.Changeset.html#traverse_errors/2
   def translate_errors(changeset) do
     Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
       Enum.reduce(opts, msg, fn {key, value}, acc ->
         String.replace(acc, "%{#{key}}", to_string(value))
       end)
     end)
-    |> Enum.flat_map(fn {key, values} ->
-      Enum.map(values, fn value ->
-        {key, "#{key} #{value}"}
-      end)
-    end)
-  end
-
-  defp format_error({title, msg}) do
-    %{
-      message_title: title,
-      message: msg
-    }
   end
 end
